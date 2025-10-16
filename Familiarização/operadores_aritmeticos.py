0d2bc3fd59a94b44bc6e8c37ce1db634cbea1943
José Jonhson B.T <josejonhsondev@gmail.com>
2025-10-16 14:31:10 -0300

    Operadores Aritméticos

--- Familiarização/operadores_aritmeticos.py
+++ Familiarização/operadores_aritmeticos.py
@@ -1,1 +1,19 @@
-# Ainda não tem nada aqui ... chegou cedo ... kkkk
+ # os operadores aritméticos são:
+# + soma
+# - subtração
+# * multiplicação
+# / divisão com resultado decimal
+# // divisão inteira (descarta as casas decimais)
+# % módulo (resto da divisão)
+# ** exponenciação (potência)
+
+primeiro_numero = 12
+segundo_numero = 10
+
+print('Soma', primeiro_numero + segundo_numero)
+print('Subtração', primeiro_numero - segundo_numero)
+print('Multiplicação', primeiro_numero * segundo_numero)
+print('Divisão com casa decimal', primeiro_numero / segundo_numero)
+print('Divisão sem casa decimal', primeiro_numero // segundo_numero)
+print('Módulo', primeiro_numero % segundo_numero)
+print('Exponenciação', primeiro_numero ** segundo_numero)
